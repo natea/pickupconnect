773277fe7517a7c6a0d8f2dbb32cc960e42bbff5
Nate Aune <natea@jazkarta.com>
2011-10-15 16:31:19 -0400

    need auth for admin

--- settings.py
+++ settings.py
@@ -105,8 +105,13 @@ MIDDLEWARE_CLASSES = (
 )
 
 TEMPLATE_CONTEXT_PROCESSORS = (
+    'django.core.context_processors.request',
+    'django.contrib.auth.context_processors.auth',
+    'django.core.context_processors.debug',
+    'django.core.context_processors.i18n',
     'django.core.context_processors.media',
     'django.core.context_processors.static',
+    'django.contrib.messages.context_processors.messages',
 )
 
 ROOT_URLCONF = 'urls'
