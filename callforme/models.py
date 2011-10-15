ee0e9713d47eb12b1cb5a615a9ddcf1ea3f65458
Steve Hershman <hershman@gmail.com>
2011-10-15 17:23:39 -0400

    added models

--- callforme/models.py
+++ callforme/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Contact(models.Model):
+	user = models.ForeignKey(User)
+	name = models.CharField(max_length = 200)
+	phone = models.CharField(max_length = 200)
+
+	FREQUENCY_CHOICES = (
+	        ('D', 'Daily'),
+		('W', 'Weekly'),
+		('M', 'Monthly'),
+    	)
+	frequency = models.CharField(max_length=1, choices=FREQUENCY_CHOICES)
+
+class Call(models.Model):
+	user = models.ForeignKey(User)
+	contact = models.ForeignKey(Contact)
+	time = models.DateTimeField(auto_now_add = True)
+
+class UserProfile(models.Model):
+	user = models.ForeignKey(User)
+	name = models.CharField(max_length = 200)
+	phone = models.CharField(max_length = 200)
