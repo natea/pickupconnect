8ead259a180dea3d1a10bab60ac6d102b28db219
Nate Aune <natea@jazkarta.com>
2011-10-15 16:26:44 -0400

    make the admin accessible

--- urls.py
+++ urls.py
@@ -17,7 +17,7 @@ urlpatterns = patterns('',
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', include(admin.site.urls)),
 
 #    url(r'^$', 'django_twilio.views.say', {'text': message}),
     url(r'^testsms/$', 'callme.views.testsms'),
